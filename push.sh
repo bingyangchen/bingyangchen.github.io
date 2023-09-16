af0e6bedfd95499adf6b83cded2488695653b13e
Jamison-Chen <106208004@g.nccu.edu.tw>
2023-09-16 12:56:01 +0800

    modify README

--- push.sh
+++ push.sh
@@ -2,9 +2,6 @@
 if [[ -z $1 ]]; then
     echo Please provide the commit message.
 else
-    # Remember to switch to the corresponding username and email for this repo
-    # git config user.name "Jamison Chen"
-    # git config user.email "106208004@g.nccu.edu.tw"
     git add .
     git commit -m "$1"
     git push origin master
