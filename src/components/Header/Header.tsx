6f0814a224dff218cd065e965146645d5d666912
Bing-Yang Chen <bryan.chen.429@gmail.com>
2026-06-10 08:30:02 +0800

    Refactor Header component and styles for improved layout and responsiveness
    - Simplified Header component by removing unused state and side menu functionality.
    - Updated Header.module.scss to enhance layout, including adjustments to flex properties and media queries for better responsiveness.
    - Refined NavTab styles for consistent appearance across different screen sizes.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,12 @@
 import styles from "./Header.module.scss";
 
-import { ReactElement, useCallback, useEffect, useMemo, useState } from "react";
+import { ReactElement, useEffect, useMemo, useState } from "react";
 
-import { Logo, NavTab, RoundButton } from "..";
+import { Logo, NavTab } from "..";
 import { useTranslation } from "../../i18n/context";
-import {
-  IconBlogText,
-  IconFilePerson,
-  IconHomeblank,
-  IconList,
-  IconMedia,
-} from "../../icons";
 
 export default function Header() {
   const { dictionary } = useTranslation();
-  const [isSideMenuActive, setIsSideMenuActive] = useState(false);
   const [isHeaderTransparent, setIsHeaderTransparent] = useState(true);
 
   useEffect(() => {
@@ -33,28 +25,24 @@ export default function Header() {
   const subpageTabs = useMemo(
     (): ReactElement[] => [
       <NavTab
-        tabIcon={<IconHomeblank sideLength="100%" />}
         tabName={dictionary.header.home}
         to="#home"
         onClick={() => {}}
         key="home"
       />,
       <NavTab
-        tabIcon={<IconFilePerson sideLength="100%" />}
         tabName={dictionary.header.about}
         to="#about"
         onClick={() => {}}
         key="about"
       />,
       <NavTab
-        tabIcon={<IconMedia sideLength="100%" />}
         tabName={dictionary.header.projects}
         to="#projects"
         onClick={() => {}}
         key="projects"
       />,
       <NavTab
-        tabIcon={<IconBlogText sideLength="100%" />}
         tabName={dictionary.header.blog}
         to="#blog"
         onClick={() => {}}
@@ -68,37 +56,10 @@ export default function Header() {
     ? `${styles.main} ${styles.transparent}`
     : styles.main;
 
-  const showSideMenu = useCallback(() => {
-    setIsSideMenuActive(true);
-  }, []);
-
-  const hideSideMenu = useCallback(() => {
-    setIsSideMenuActive(false);
-  }, []);
-
   return (
-    <>
-      <header className={mainClass}>
-        <Logo size="s" />
-        <div className={styles.nav_bar_tab_container}>{subpageTabs}</div>
-        <div className={styles.list_button_outer}>
-          <RoundButton onClick={showSideMenu}>
-            <IconList />
-          </RoundButton>
-        </div>
-      </header>
-      <div
-        className={styles.side_menu + (isSideMenuActive ? ` ${styles.active}` : "")}
-        onClick={hideSideMenu}
-      >
-        {subpageTabs}
-      </div>
-      <div
-        className={
-          styles.side_menu_background + (isSideMenuActive ? ` ${styles.active}` : "")
-        }
-        onClick={hideSideMenu}
-      />
-    </>
+    <header className={mainClass}>
+      <Logo size="s" />
+      <div className={styles.nav_bar_tab_container}>{subpageTabs}</div>
+    </header>
   );
 }
